60c8bf5d023b49a595e967467f166a995738083e
Stella Chen <stella@StelladeMacBook-Pro.local>
2019-04-22 14:28:36 -0400

    added the project files

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// get the references to the html elements:
+const navItems = document.querySelectorAll("nav a");
+const container = document.querySelector(".container");
+
+let path = "http://localhost:8888/hot-topics/partials/home.html";
+
+// handling clicking
+function handleClick(ev) {
+    
+    // prevetns link from redirecting
+    ev.preventDefault();
+    
+    // current url is collected:
+    path = ev.target.href;
+    
+    handleAjax(path);
+}
+
+// handling fetching data
+function handleAjax(urlValue) {
+    fetch(urlValue)
+        .then(function (rsp) {
+            return rsp.text();
+        })
+        .then(function (data) {
+            container.innerHTML = data;
+        })
+}
+
+handleAjax(path);
+
+navItems[0].addEventListener("click", handleClick);
+navItems[1].addEventListener("click", handleClick);
